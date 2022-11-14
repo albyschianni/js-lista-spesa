66603ebf29b1a003765321ebcfc4444a169e6350
albyschianni <alberto.schianni@gmail.com>
2022-11-14 17:26:56 +0100

    for

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).
+
+const listaSpesa = ["acqua","pane","latte","carne","uova"];
+
+
+for (i = 0; i < listaSpesa.length; i++){
+    // let lista = document.getElementById("lista-spesa")
+    let oggetto = document.createElement("li");
+    document.getElementById("lista-spesa").appendChild(oggetto);
+    oggetto.innerText = (listaSpesa[i]);
+    
+}
